7a58539716fd407b847dc4a9bbe7673c519a7b19
Chris J. Hou <40023650+Codth@users.noreply.github.com>
2020-07-14 22:09:05 -0400

    change dg

--- models/db.js
+++ models/db.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 
 
-const uri = "mongodb+srv://dbUser:Fuckyou123%40@cluster0-uox64.mongodb.net/dbUser?retryWrites=true&w=majority";
-mongoose.connect(uri, {
-    useNewUrlParser: true
-});
+
+// const uri = "mongodb+srv://dbUser:Fuckyou123%40@cluster0-uox64.mongodb.net/dbUser?retryWrites=true&w=majority";
+// mongoose.connect(uri, {
+//     useNewUrlParser: true
+// });
 
  
-//
-// mongoose.connect('mongodb://localhost:27017/BlogDB',{ useNewUrlParser: true,useUnifiedTopology: true }, (err) => {
-//     if (!err) { console.log('MongoDB Connection Succeeded.') }
-//     else { console.log('Error in DB connection : ' + err) }
-// });
+
+mongoose.connect('mongodb://localhost:27017/BlogDB',{ useNewUrlParser: true,useUnifiedTopology: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
+});
 
 require('./blog.model');
